6349a1584928254ecbc440ace70b191e4c20a32a
vivek <>
2022-02-17 15:52:06 +0000

    intro

--- .ccls-cache/@home@runner@DSA/main.cpp
+++ .ccls-cache/@home@runner@DSA/main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 int main(){
 
  
+ int A = 'a';
+  string name;
+  string designation;
 
+  
+  cout<<"Please Enter Your Name"<<endl;
+  cin>>name;
+  cout<<"Enter Your Designation"<<endl;
+  cin>>designation;
+  cout<<"Your Name : "<<name<<endl<<"Your Designation : "<<designation;
 
   
 }
