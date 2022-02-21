a61f1c75693ebf185c5444825c5bb7a28c4f9505
Vivek Kuvadiya <65214194+vivekkuvadiya@users.noreply.github.com>
2022-02-21 18:10:52 +0000

    primenumber & fibonacci

--- .ccls-cache/@home@runner@DSA/main.cpp
+++ .ccls-cache/@home@runner@DSA/main.cpp
@@ -3,5 +3,44 @@ using namespace std;
 
 int main(){
 
+
+/*
+  //fibonacci
+
+  int n;
+  cout<<"Enter N Number : ";
+  cin>>n;
+
+  int a = 0;
+  int b = 1;
+
+  for(int i = 1; i<=n;i++){
+    int nextSum = a + b;
+    cout<<nextSum<<endl;
+    a = b;
+    b = nextSum;
+  }
+
+*/
+
+//prime number
+
+int a;
+cout<<"Enter number : ";
+cin>>a;
+
+  bool isPrime = true;
+
+  for(int i = 2; i<=(a-1); i++){
+    if(a%i==0){
+      isPrime = false;
+      break;
+    }
+  }
+  if(isPrime){
+    cout<<a<<" is Prime Number";
+  }else {
+    cout<<a<<" is not Prime Number";
+  }
   
 }
