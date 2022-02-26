ad9c8c2c4bc6a97c71836a78b55a568e51321c8e
Vivek Kuvadiya <65214194+vivekkuvadiya@users.noreply.github.com>
2022-02-26 07:15:10 +0000

    descimal to bynary

--- .ccls-cache/@home@runner@DSA/main.cpp
+++ .ccls-cache/@home@runner@DSA/main.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main(){
 
+
+  // descimal to bynary
+  
+  int a;
+  cout<<"Enter Number : ";
+  cin>>a;
+
+  int byteArray[32];
+
+  int i = 0;
+  while(a>0){
+    byteArray[i] = a % 2;
+    a = a/2;
+    i++;
+  }
+
+  for(int j = (i - 1); j>=0;j--){
+    cout<<byteArray[j];
+  }
+  
   
 }
